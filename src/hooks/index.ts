395df1c645ada41de85dd38428d2485e31843709
robin-lebhar-ifs <80712164+robin-lebhar-ifs@users.noreply.github.com>
2021-08-05 14:34:24 +0200

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
-import { AnyAction, Dispatch } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../redux';
 

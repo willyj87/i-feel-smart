01284c31105b887aa0ce0082ea9ead248024aebf
robin-lebhar-ifs <80712164+robin-lebhar-ifs@users.noreply.github.com>
2021-08-05 14:20:46 +0200

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,5 +3,5 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../redux';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = (): Dispatch<AnyAction> => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>() // Export a hook that can be reused to resolve types
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

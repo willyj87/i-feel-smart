b48b125f16c0bc0a9d55594cb94631115819bb10
Willy NOUBISSIE <willyjunior91@gmail.com>
2021-08-22 19:22:06 +0200

    feat(ticket-xxx): add api and axios lib

--- src/containers/counter/index.tsx
+++ src/containers/counter/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchProgram } from '../../api/epg';
+import { fetchContent } from '../../api/vod';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { decrement, increment } from '../../redux/counter';
 
 export function Counter(): JSX.Element {
+    useEffect(() => {
+        (async () => {
+            const epg = await fetchProgram('296');
+            const vod = await fetchContent('66')
+            console.log({programs: epg});
+            console.log({vod});
+            return epg;
+        })()
+    }, [])
     const count = useAppSelector((state) => state.counter.value);
     const dispatch = useAppDispatch();
     return (
